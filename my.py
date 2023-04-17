888175442659c4cf889d76a6a82d051b1393c18c
tech-in-tech <103058497+tech-in-tech@users.noreply.github.com>
2023-04-17 13:44:27 +0530

    Add files via upload

---
+++ my.py
@@ -0,0 +1,31 @@
+# ans 1
+a = []
+
+# ans 2
+b = [1,2,10,40,3,4,5]
+b.append(6)
+print(b)
+
+# ans 3
+print(b[-1])
+
+# ans 4
+print(b[::-1])
+
+# ans 5
+b.sort()
+print(b)
+
+# ans 6
+movie = ["past","present","future"]
+print(movie[1])
+
+# ans 7
+animal = ["lamborgani","farari","jaguar"]
+animal.append("firefox")
+print(animal)
+
+# ans 8
+city = ["austin","san francisco","new york",'bengaluru']
+print(city.index("new york"))
+
